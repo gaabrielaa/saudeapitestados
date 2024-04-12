7845dbbdaf41a0b596d8c1efad0b2b49227d64cf
Gabriella <gabriella.novaes@portalsesisp.org.br>
2024-04-12 11:17:44 -0300

    apitestados

---
+++ Models/ClasseMed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIsaude.Models;
+
+public partial class ClasseMed
+{
+    public int CodClass { get; set; }
+
+    public string NomeClasse { get; set; } = null!;
+
+    public virtual ICollection<MatMed> MatMeds { get; } = new List<MatMed>();
+}
